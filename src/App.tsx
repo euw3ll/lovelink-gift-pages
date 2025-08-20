05ba289fbd0b96dc3df7e8dc7feaf424eec9aeb6
Wendrell Antoneli <wendrell.antoneli@gmail.com>
2025-08-19 23:19:43 -0300

    Use Auth component for login and register routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 
 const queryClient = new QueryClient();
